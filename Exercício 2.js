12d190d08627cfc25602e66991b0e6f78c2f4e29
Gabriel9706374 <gabriel9706374@rede.ulbra.br>
2026-06-19 18:36:21 -0300

    Add files via upload

---
+++ Exercício 2.js
@@ -0,0 +1,20 @@
+let aluno1 = "Vitor"
+let aluno1_nota1 = 6
+let aluno1_nota2 = 9
+let resultado_aluno1 = (aluno1_nota1 + aluno1_nota2) / 2
+
+console.log(aluno1, resultado_aluno1 >= 7)
+
+let aluno2 = "Daniel"
+let aluno2_nota1 = 9
+let aluno2_nota2 = 4
+let resultado_aluno2 = (aluno2_nota1 + aluno2_nota2) / 2
+
+console.log(aluno2, resultado_aluno2 >= 7)
+
+let aluno3 = "Paulo"
+let aluno3_nota1 = 3
+let aluno3_nota2 = 5
+let resultado_aluno3 = (aluno3_nota1 + aluno3_nota2) / 2
+
+console.log(aluno3, resultado_aluno3 >= 7)

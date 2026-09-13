4fe0f0543e9f4f51a353c5898f7e39989db70e52
Gabriel9706374 <gabriel9706374@rede.ulbra.br>
2026-09-13 20:59:26 -0300

    Add files via upload

--- src/main/java/com/mycompany/teladelogin/TelaDeLogin.java
+++ src/main/java/com/mycompany/teladelogin/TelaDeLogin.java
@@ -1,1 +1,10 @@
+package com.mycompany.teladelogin;
 
+import view.Tela;
+
+public class TelaDeLogin {
+
+    public static void main(String[] args) {
+        new Tela().setVisible(true);
+    }
+}
